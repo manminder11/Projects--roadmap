7a6b5602eb67be0e4a9ccfb4bf0600a7afc0ebb9
Manminder <ManminderSingh@edu.sait.ca>
2024-12-19 19:45:13 -0700

    perfect

--- Java Projects/Mortgage calculator/src/Mortgage_calculator.java
+++ Java Projects/Mortgage calculator/src/Mortgage_calculator.java
@@ -8,33 +8,32 @@ public class Mortgage_calculator {
 
 
         Scanner scanner = new Scanner(System.in);
-        int principal_amount =  scanner.nextInt();
-        System.out.println("Principal:" + principal_amount);
 
-        Scanner Annual_int_rate = new Scanner(System.in);
-        double AnnualIntrestrate = Annual_int_rate.nextInt();
-        AnnualIntrestrate = AnnualIntrestrate/12;
-       AnnualIntrestrate = AnnualIntrestrate*AnnualIntrestrate;
-       System.out.println("MontlyIntrestrate" + AnnualIntrestrate);
-
-
-       Scanner time = new Scanner(System.in);
-        int period =  time.nextInt();
-        period = period/12; // montly
-        System.out.println("Period (Years):" + period
-        );
+        System.out.println("Principal:");
+       double principal_amount =  scanner.nextInt();
 
 
 
-        Scanner total_payments = new Scanner(System.in);
-        int total_no_of_payments = total_payments.nextInt();
-        total_no_of_payments= total_no_of_payments* total_no_of_payments;
-        total_no_of_payments = Math.multiplyExact(total_no_of_payments , 12);
+       System.out.println("MontlyIntrestrate:");
+        double AnnualIntrestrate = scanner.nextDouble();
+        double MontlyIntrestrate = AnnualIntrestrate/100/12;
+
+       System.out.println("period (Years):");
+       int years  = scanner.nextInt();
+       int total_pay = years/12;
+
+
+
+
+        double numenator = MontlyIntrestrate * Math.pow(1+ MontlyIntrestrate , total_pay);
+        double denominator = Math.pow(1+ MontlyIntrestrate , total_pay) - 1;
+        double monthlypay = principal_amount * (numenator/denominator);
 
 
         // we calculate the mortgage here
 
-       System.out.println(principal_amount*(AnnualIntrestrate*(1+AnnualIntrestrate*total_no_of_payments/1+AnnualIntrestrate*total_no_of_payments-1)));
+       System.out.println(monthlypay);
+
 
 
 
