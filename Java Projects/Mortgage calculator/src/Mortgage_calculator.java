b8852dae41853682c0fcd90bfc868c6198cbb151
Manminder <ManminderSingh@edu.sait.ca>
2024-12-26 10:44:29 -0700

    lines

--- Java Projects/Mortgage calculator/src/Mortgage_calculator.java
+++ Java Projects/Mortgage calculator/src/Mortgage_calculator.java
@@ -4,15 +4,10 @@ public class Mortgage_calculator {
    public static void main(String[]args){
 
 
-
-
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Principal:");
        double principal_amount =  scanner.nextDouble();
-
-
 
        System.out.println("MontlyIntrestrate:");
         double AnnualIntrestrate = scanner.nextDouble();
@@ -23,21 +18,13 @@ public class Mortgage_calculator {
        int total_pay = years/12;
 
 
-
-
         double numenator = MontlyIntrestrate * Math.pow(1+ MontlyIntrestrate , total_pay);
         double denominator = Math.pow(1+ MontlyIntrestrate , total_pay) - 1;
         double monthlypay = principal_amount * (numenator/denominator);
 
-
         // we calculate the mortgage here
 
        System.out.println(monthlypay);
-
-
-
-
-
 
     }
 }
